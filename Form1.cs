6adf3a8bd643d8b293157b57b5d4c0a484c3687b
ROBERT SEWELL <P214430@tafe.wa.edu.au>
2025-04-17 11:21:22 +0800

    The "Load Astrometric Data" button was coded and now will clear all previous text in the list box, fill our array with random data, and print the array to the list box

--- Form1.cs
+++ Form1.cs
@@ -29,18 +29,34 @@ namespace AstroData
 
         // This is where we declare the global variable that will control the max number of values
         // that can be in our array of astrometric data
-        static int numbOfDataValues = 20;
+        static int dataValueQty = 20;
 
         // This is where we declare our array of astrometric data
-        int[] dataArray = new int[numbOfDataValues];
+        int[] dataArray = new int[dataValueQty];
 
 
 
         private void ButtonRandomData_Click(object sender, EventArgs e)
         {
+            // Clear any previous data that was written to the list box
+            ListBoxData.Items.Clear();
+
             // Here we will create a random number to add to our array of data values,
             // and we will do this until the array has the max number of entries.
+            Random randValue = new Random();
+            
+            // This is the for loop that will add random values to 'dataArray' until it is full
+            for (int i = 0; i < dataValueQty; i++)
+            {
+                dataArray[i] = randValue.Next(100);
+            }
 
+            // Then the array will be displayed in the listbox named "ListBoxData" via a 
+            // for loop
+            for (int i = 0; i < dataValueQty; i++)
+            {
+                ListBoxData.Items.Add(dataArray[i]);
+            }
 
         }
 
