4f51ba5e56f3860480aff1ac98540577c91c9bfa
ROBERT SEWELL <P214430@tafe.wa.edu.au>
2025-04-17 10:11:50 +0800

    Added button for binary search and text box for input for the binary search button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.ButtonRandomData = new System.Windows.Forms.Button();
             this.ListBoxData = new System.Windows.Forms.ListBox();
+            this.ButtonBubbleSort = new System.Windows.Forms.Button();
+            this.TextBoxSearch = new System.Windows.Forms.TextBox();
+            this.ButtonBinarySearch = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ButtonRandomData
@@ -49,16 +52,45 @@
             this.ListBoxData.Size = new System.Drawing.Size(139, 303);
             this.ListBoxData.TabIndex = 1;
             // 
+            // ButtonBubbleSort
+            // 
+            this.ButtonBubbleSort.Location = new System.Drawing.Point(176, 52);
+            this.ButtonBubbleSort.Name = "ButtonBubbleSort";
+            this.ButtonBubbleSort.Size = new System.Drawing.Size(75, 23);
+            this.ButtonBubbleSort.TabIndex = 2;
+            this.ButtonBubbleSort.Text = "Bubble Sort";
+            this.ButtonBubbleSort.UseVisualStyleBackColor = true;
+            // 
+            // TextBoxSearch
+            // 
+            this.TextBoxSearch.Location = new System.Drawing.Point(176, 160);
+            this.TextBoxSearch.Name = "TextBoxSearch";
+            this.TextBoxSearch.Size = new System.Drawing.Size(86, 20);
+            this.TextBoxSearch.TabIndex = 3;
+            // 
+            // ButtonBinarySearch
+            // 
+            this.ButtonBinarySearch.Location = new System.Drawing.Point(176, 131);
+            this.ButtonBinarySearch.Name = "ButtonBinarySearch";
+            this.ButtonBinarySearch.Size = new System.Drawing.Size(86, 23);
+            this.ButtonBinarySearch.TabIndex = 4;
+            this.ButtonBinarySearch.Text = "Binary Search";
+            this.ButtonBinarySearch.UseVisualStyleBackColor = true;
+            // 
             // AstroDataForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(502, 426);
+            this.ClientSize = new System.Drawing.Size(273, 404);
+            this.Controls.Add(this.ButtonBinarySearch);
+            this.Controls.Add(this.TextBoxSearch);
+            this.Controls.Add(this.ButtonBubbleSort);
             this.Controls.Add(this.ListBoxData);
             this.Controls.Add(this.ButtonRandomData);
             this.Name = "AstroDataForm";
             this.Text = "AstroData";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -66,6 +98,9 @@
 
         private System.Windows.Forms.Button ButtonRandomData;
         private System.Windows.Forms.ListBox ListBoxData;
+        private System.Windows.Forms.Button ButtonBubbleSort;
+        private System.Windows.Forms.TextBox TextBoxSearch;
+        private System.Windows.Forms.Button ButtonBinarySearch;
     }
 }
 
