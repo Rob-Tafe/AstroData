70e41a0fe83fe9019028212b378e3845fbcff621
ROBERT SEWELL <P214430@tafe.wa.edu.au>
2025-04-17 11:04:49 +0800

    Declared a global variable for max values in our data array, and declared our global variable for the data array itself

--- Form1.cs
+++ Form1.cs
@@ -27,10 +27,20 @@ namespace AstroData
 
 
 
+        // This is where we declare the global variable that will control the max number of values
+        // that can be in our array of astrometric data
+        static int numbOfDataValues = 20;
+
+        // This is where we declare our array of astrometric data
+        int[] dataArray = new int[numbOfDataValues];
+
+
+
         private void ButtonRandomData_Click(object sender, EventArgs e)
         {
             // Here we will create a random number to add to our array of data values,
             // and we will do this until the array has the max number of entries.
+
 
         }
 
