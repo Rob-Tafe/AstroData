d64bb4d5ecc729f1bd1949d881ec9bcdd57882ee
Rob-Tafe <p214430@tafe.wa.edu.au>
2025-05-05 15:32:13 +0800

    Added missing title block

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿// Program: AstroData
+//
+// Description: A Forms application that will receive data input and display the data in a list box, with options to sort the data, and search for a specific value based on user input.
+//
+// Author: Robert Sewell
+//
+// Date: 17 / 04 / 2025
+//
+// Version: 0.1
+
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
