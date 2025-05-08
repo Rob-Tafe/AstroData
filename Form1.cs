aa857a9a793a4fdf8b4c7355bd6a2b2e988a1009
ROBERT SEWELL <P214430@tafe.wa.edu.au>
2025-05-08 11:24:49 +0800

    Header updated for sprint 2

--- Form1.cs
+++ Form1.cs
@@ -2,13 +2,19 @@
 //
 // Description: A Forms application that will receive data input and display
 //              the data in a list box, with options to sort the data, and search
-//              for a specific value based on user input.
+//              for a specific value based on user input. There will also be buttons
+//              for a sequential search, and four mathematical operations that will
+//              show the user the mid-extreme, mode, average, and range of the data.
 //
 // Author:      Robert Sewell
 //
-// Date:        17 / 04 / 2025
+// Team Name:   AstroData Team
 //
-// Version:     0.1
+// Sprint #:    2
+//
+// Date:        08 / 05 / 2025
+//
+// Version:     0.2
 
 
 
